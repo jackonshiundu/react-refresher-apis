852f3c247259497085d06f1fd4b7c9aeae0c35ad
jackonshiundu <jackonshiundu2019@gmail.com>
2025-01-04 22:14:45 +0300

    fetching-the-movies

--- src/Components/Moviecard.jsx
+++ src/Components/Moviecard.jsx
@@ -8,7 +8,10 @@ export function Moviecard({ movie }) {
   return (
     <div className="movie-card">
       <div className="movie-poster">
-        <img src={movie.url} alt={movie.title} />
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
         <div className="movie-overlay">
           <button className="favourite-btn" onClick={onLike}>
             ❤
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,43 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Moviecard } from "../Components/Moviecard";
 import "../css/Home.css";
+import { getPopularMolvies, searchMovies } from "../services/api";
 
 function Home() {
-  const movies = [
-    {
-      id: 1,
-      title: "Hello Hello",
-      release_date: "12-May-2025",
-      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIM0-w43ChyvBSM5cXykYx5NV-Yl0WEX6Aqw&s",
-    },
-    {
-      id: 2,
-      title: "The Great Adventure",
-      release_date: "15-Jul-2023",
-      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp4JgQ-KThpn8vLaZG6kQK3t5VhO8E_R-RoQ&s",
-    },
-    {
-      id: 3,
-      title: "Mystery at Midnight",
-      release_date: "22-Sep-2024",
-      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp7DgmX72vQ_WljszHLbp1lQbu9o0LUXfl0g&s",
-    },
-    {
-      id: 4,
-      title: "City of Dreams",
-      release_date: "5-Dec-2023",
-      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBUu2bNoi0hwdYnFhW8Nweirj5dXg2NzQbPA&s",
-    },
-    {
-      id: 5,
-      title: "Journey to the Stars",
-      release_date: "1-Jan-2026",
-      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSevMZTgmYxxQbMYxxOExA7kIdtdfng1cpa7A&s",
-    },
-  ];
   const [searchQuery, setSearchQuery] = useState("");
-  const handleOnSubmit = (e) => {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        setLoading(true);
+        const popularMovies = await getPopularMolvies();
+        setMovies(popularMovies);
+      } catch (error) {
+        console.log(error);
+        setError("Failed to load");
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadPopularMovies();
+  }, []);
+
+  const handleOnSubmit = async (e) => {
     e.preventDefault();
+    if (!searchQuery.trim()) return;
+    if (loading) return;
+    setLoading(true);
+    try {
+      const searchResult = await searchMovies(searchQuery);
+      setMovies(searchResult);
+      setError(null);
+    } catch (error) {
+      setError("Failed to fetch the searched text");
+    } finally {
+      setLoading(false);
+    }
   };
   return (
     <div className="home">
@@ -55,14 +56,19 @@ function Home() {
           Search
         </button>
       </form>
-      <div className="movie-grid">
-        {movies.map(
-          (movie) =>
-            movie.title.toLowerCase().includes(searchQuery) && (
-              <Moviecard movie={movie} index={movie.id} />
-            )
-        )}
-      </div>
+      {error && <div className="error-message">{error}</div>}
+      {loading ? (
+        <div classname="loading"></div>
+      ) : (
+        <div className="movie-grid">
+          {movies.map((movie) => (
+            /*               movie.title.toLowerCase().includes(searchQuery) && (
+                <Moviecard movie={movie} index={movie.id} />
+                ) */
+            <Moviecard movie={movie} index={movie.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
